1ecf3a506eefbe7544c95f83c1639c92dc4cd25a
Vladimir Nenahov <nenahov@gmail.com>
2016-04-17 22:13:21 +0300

    HelloWorld

--- src/com/thecherno/flappy/Main.java
+++ src/com/thecherno/flappy/Main.java
@@ -1,14 +1,41 @@
 package com.thecherno.flappy;
 
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL13.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_NO_ERROR;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.GL_VERSION;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glGetError;
+import static org.lwjgl.opengl.GL11.glGetString;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE1;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
-import java.nio.ByteBuffer;
-
-import org.lwjgl.glfw.GLFWvidmode;
-import org.lwjgl.opengl.GLContext;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 
 import com.thecherno.flappy.graphics.Shader;
 import com.thecherno.flappy.input.Input;
@@ -16,120 +43,136 @@ import com.thecherno.flappy.level.Level;
 import com.thecherno.flappy.maths.Matrix4f;
 
 public class Main implements Runnable {
-	
-	private int width = 1280;
-	private int height = 720;
-	
-	private Thread thread;
-	private boolean running = false;
-	
-	private long window;
-	
-	private Level level;
-	
-	public void start() {
-		running = true;
-		thread = new Thread(this, "Game");
-		thread.start();
-	}
-	
-	private void init() {
-		if (glfwInit() != GL_TRUE) {
-			System.err.println("Could not initialize GLFW!");
-			return;
-		}
-		
-		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-		window = glfwCreateWindow(width, height, "Flappy", NULL, NULL);
-		if (window == NULL) {
-			System.err.println("Could not create GLFW window!");
-			return;
-		}
-		
-		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-		glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - width) / 2, (GLFWvidmode.height(vidmode) - height) / 2);
-		
-		glfwSetKeyCallback(window, new Input());
-		
-		glfwMakeContextCurrent(window);
-		glfwShowWindow(window);
-		GLContext.createFromCurrent();
-		
- 		glEnable(GL_DEPTH_TEST);
-		glActiveTexture(GL_TEXTURE1);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		System.out.println("OpenGL: " + glGetString(GL_VERSION));
-		Shader.loadAll();
-		
-		Matrix4f pr_matrix = Matrix4f.orthographic(-10.0f, 10.0f, -10.0f * 9.0f / 16.0f, 10.0f * 9.0f / 16.0f, -1.0f, 1.0f);
-		Shader.BG.setUniformMat4f("pr_matrix", pr_matrix);
-		Shader.BG.setUniform1i("tex", 1);
-		
-		Shader.BIRD.setUniformMat4f("pr_matrix", pr_matrix);
-		Shader.BIRD.setUniform1i("tex", 1);
-		
-		Shader.PIPE.setUniformMat4f("pr_matrix", pr_matrix);
-		Shader.PIPE.setUniform1i("tex", 1);
-		
-		level = new Level();
-	}
-	
-	public void run() {
-		init();
-		
-		long lastTime = System.nanoTime();
-		double delta = 0.0;
-		double ns = 1000000000.0 / 60.0;
-		long timer = System.currentTimeMillis();
-		int updates = 0;
-		int frames = 0;
-		while (running) {
-			long now = System.nanoTime();
-			delta += (now - lastTime) / ns;
-			lastTime = now;
-			if (delta >= 1.0) {
-				update();
-				updates++;
-				delta--;
-			}
-			render();
-			frames++;
-			if (System.currentTimeMillis() - timer > 1000) {
-				timer += 1000;
-				System.out.println(updates + " ups, " + frames + " fps");
-				updates = 0;
-				frames = 0;
-			}
-			if (glfwWindowShouldClose(window) == GL_TRUE)
-				running = false;
-		}
-		
-		glfwDestroyWindow(window);
-		glfwTerminate();
-	}
-	
-	private void update() {
-		glfwPollEvents();
-		level.update();
-		if (level.isGameOver()) {
-			level = new Level();
-		}
-	}
-	
-	private void render() {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		level.render();
-		
-		int error = glGetError();
-		if (error != GL_NO_ERROR)
-			System.out.println(error);
-		
-		glfwSwapBuffers(window);
-	}
 
-	public static void main(String[] args) {
-		new Main().start();
-	}
+    private int width = 1280;
+    private int height = 720;
+
+    private Thread thread;
+    private boolean running = false;
+
+    private long window;
+
+    private Level level;
+    private Input cbfun;
+
+    public void start() {
+        running = true;
+        thread = new Thread(this, "Game");
+        thread.start();
+    }
+
+    private void init() {
+        if (glfwInit() != GL_TRUE) {
+            System.err.println("Could not initialize GLFW!");
+            return;
+        }
+
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+        window = glfwCreateWindow(width, height, "Flappy", NULL, NULL);
+        if (window == NULL) {
+            System.err.println("Could not create GLFW window!");
+            return;
+        }
+
+        // Get the resolution of the primary monitor
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        // Center our window
+        glfwSetWindowPos(
+            window,
+            (vidmode.width() - width) / 2,
+            (vidmode.height() - height) / 2);
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+
+        cbfun = new Input();
+        glfwSetKeyCallback(window, cbfun);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+
+        // GLContext.createFromCurrent();
+
+        glEnable(GL_DEPTH_TEST);
+        glActiveTexture(GL_TEXTURE1);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        System.out.println("OpenGL: " + glGetString(GL_VERSION));
+        Shader.loadAll();
+
+        Matrix4f pr_matrix = Matrix4f.orthographic(-10.0f, 10.0f, -10.0f * 9.0f / 16.0f, 10.0f * 9.0f / 16.0f, -1.0f, 1.0f);
+        Shader.BG.setUniformMat4f("pr_matrix", pr_matrix);
+        Shader.BG.setUniform1i("tex", 1);
+
+        Shader.BIRD.setUniformMat4f("pr_matrix", pr_matrix);
+        Shader.BIRD.setUniform1i("tex", 1);
+
+        Shader.PIPE.setUniformMat4f("pr_matrix", pr_matrix);
+        Shader.PIPE.setUniform1i("tex", 1);
+
+        level = new Level();
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        long lastTime = System.nanoTime();
+        double delta = 0.0;
+        double ns = 1000000000.0 / 60.0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1.0) {
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println(updates + " ups, " + frames + " fps");
+                updates = 0;
+                frames = 0;
+            }
+            if (glfwWindowShouldClose(window) == GL_TRUE)
+                running = false;
+        }
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    private void update() {
+        glfwPollEvents();
+        level.update();
+        if (level.isGameOver()) {
+            level = new Level();
+        }
+    }
+
+    private void render() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        level.render();
+
+        int error = glGetError();
+        if (error != GL_NO_ERROR)
+            System.out.println(error);
+
+        glfwSwapBuffers(window);
+    }
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
 
 }
--- src/com/thecherno/flappy/level/Bird.java
+++ src/com/thecherno/flappy/level/Bird.java
@@ -1,6 +1,6 @@
 package com.thecherno.flappy.level;
 
-import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
 
 import com.thecherno.flappy.graphics.Shader;
 import com.thecherno.flappy.graphics.Texture;
@@ -11,67 +11,66 @@ import com.thecherno.flappy.maths.Vector3f;
 
 public class Bird {
 
-	private float SIZE = 1.0f;
-	private VertexArray mesh;
-	private Texture texture;
-	
-	private Vector3f position = new Vector3f();
-	private float rot;
-	private float delta = 0.0f;
-	
-	public Bird() {
-		float[] vertices = new float[] {
-			-SIZE / 2.0f, -SIZE / 2.0f, 0.2f,
-			-SIZE / 2.0f,  SIZE / 2.0f, 0.2f,
-			 SIZE / 2.0f,  SIZE / 2.0f, 0.2f,
-			 SIZE / 2.0f, -SIZE / 2.0f, 0.2f
-		};
-			
-		byte[] indices = new byte[] {
-			0, 1, 2,
-			2, 3, 0
-		};
-		
-		float[] tcs = new float[] {
-			0, 1,
-			0, 0,
-			1, 0,
-			1, 1
-		};
-		
-		mesh = new VertexArray(vertices, indices, tcs);
-		texture = new Texture("res/bird.png");
-	}
-	
-	public void update() {
-		position.y -= delta;
-		if (Input.isKeyDown(GLFW_KEY_SPACE)) 
-			delta = -0.15f;
-		else
-			delta += 0.01f;
-		
-		rot = -delta * 90.0f;
-	}
-	
-	public void fall() {
-		delta = -0.15f;
-	}
-	
-	public void render() {
-		Shader.BIRD.enable();
-		Shader.BIRD.setUniformMat4f("ml_matrix", Matrix4f.translate(position).multiply(Matrix4f.rotate(rot)));
-		texture.bind();
-		mesh.render();
-		Shader.BIRD.disable();
-	}
+    private float SIZE = 1.0f;
+    private VertexArray mesh;
+    private Texture texture;
 
-	public float getY() {
-		return position.y;
-	}
+    private Vector3f position = new Vector3f();
+    private float rot;
+    private float delta = 0.0f;
 
-	public float getSize() {
-		return SIZE;
-	}
+    public Bird() {
+        float[] vertices = new float[] {
+        -SIZE / 2.0f, -SIZE / 2.0f, 0.2f,
+        -SIZE / 2.0f, SIZE / 2.0f, 0.2f,
+        SIZE / 2.0f, SIZE / 2.0f, 0.2f,
+        SIZE / 2.0f, -SIZE / 2.0f, 0.2f
+        };
 
-	
+        byte[] indices = new byte[] {
+        0, 1, 2,
+        2, 3, 0
+        };
+
+        float[] tcs = new float[] {
+        0, 1,
+        0, 0,
+        1, 0,
+        1, 1
+        };
+
+        mesh = new VertexArray(vertices, indices, tcs);
+        texture = new Texture("res/bird.png");
+    }
+
+    public void update() {
+        position.y -= delta;
+        if (Input.isKeyDown(GLFW_KEY_SPACE))
+            delta = -0.0515f;
+        else
+            delta += 0.001f;
+
+        rot = -delta * 90.0f;
+    }
+
+    public void fall() {
+        delta = -0.15f;
+    }
+
+    public void render() {
+        Shader.BIRD.enable();
+        Shader.BIRD.setUniformMat4f("ml_matrix", Matrix4f.translate(position).multiply(Matrix4f.rotate(rot)));
+        texture.bind();
+        mesh.render();
+        Shader.BIRD.disable();
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public float getSize() {
+        return SIZE;
+    }
+
 }
